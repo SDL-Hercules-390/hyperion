f6ab9eb0489bcb786b617987498da99ac8775106
Fish (David B. Trout) <fish@infidels.org>
2018-01-13 19:22:42 -0800

    How the F**K did THAT happen?!
    AutoBuildCount.h should NOT, *EVER*, be a member of the repository!!
    
    I have no idea how it got added, but it SHOULDN'T have been added!!

--- AutoBuildCount.h
+++
@@ -1,7 +0,0 @@
-#ifndef AUTOBUILDCOUNT
-#define AUTOBUILDCOUNT
-#define BUILDCOUNT_NUM  9
-#define BUILDCOUNT_STR "9"
-#define GITCTR_NUM      144
-#define GITHASH_STR    "144-g10aaedf-modified"
-#endif
