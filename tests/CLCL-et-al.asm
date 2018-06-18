134f47bf2c3b9b763415d390b122af123e5e137e
Fish (David B. Trout) <fish@infidels.org>
2018-06-18 06:01:12 -0700

    (cosmetic: correct .asm comments regarding *Testcase)

--- tests/CLCL-et-al.asm
+++ tests/CLCL-et-al.asm
@@ -27,14 +27,13 @@
 *     loadcore    "$(testpath)/CLCL-et-al.core"
 *
 *     runtest     2         # (NON-timing test duration)
-*     ##r           1fff=ff   # (enable timing tests too!)
+*     ##r           21fd=ff   # (enable timing tests too!)
 *     ##runtest     360       # (TIMING too test duration)
 *
 *     *Compare
-*     r 2000.2
+*     r 21fe.2
 *
-*     *Want  "Ending test/subtest number (NON-timing)"   0402
-*     ##*Want  "Ending test/subtest number (TIMING too)"   9401
+*     *Want  "Ending test/subtest number"   9510
 *
 *     *Done
 *
