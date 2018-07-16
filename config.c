32a2eea0be08339c9aa4c3aa956d0af3c33c643a
Peter J. Jansen <p.jansen@computer.org>
2018-07-16 15:30:48 +0200

    Fix for Absence of pthread_getcpuclockid on Apple MacOS platform

--- config.c
+++ config.c
@@ -1088,8 +1088,17 @@ int configure_cpu( int target_cpu )
             return HERRCPUOFF; /* CPU offline; create_thread failed */
         }
 
+#if defined(_POSIX_THREAD_CPUTIME) && (_POSIX_THREAD_CPUTIME >= 0)
         /* Initialise the CPU's thread clockid so that clock_gettime() can use it */
-        pthread_getcpuclockid( sysblk.cputid[ target_cpu ], &sysblk.cpuclockid[ target_cpu ]);
+        /* provided the _POSIX_THREAD_CPUTIME is supported.                       */
+        pthread_getcpuclockid(sysblk.cputid[ target_cpu ], &sysblk.cpuclockid[ target_cpu ]);
+        WRMSG(HHC00111, "I", _POSIX_THREAD_CPUTIME);
+#else
+        /* When not supported, we zero the cpuclockid, which will trigger a       */
+        /* different approach to obtain the thread CPU time in clock.c            */
+        sysblk.cpuclockid[ target_cpu ] = 0; 	
+	    WRMSG(HHC00112, "W");
+#endif /* defined(_POSIX_THREAD_CPUTIME) && (_POSIX_THREAD_CPUTIME >= 0) */	
 
         /* Find out if we are a cpu thread */
         arecpu = are_cpu_thread( &ourcpu );
