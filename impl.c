SDL-Hercules-390/hyperion

commit 79a0a17354d9f5ca9ce6032d3448aad3fca59ce2

Fix impl.c to process arguments early so that flags are set correctly.
This implements daemon mode as it should be.

Still a number of problems (sigsevs), but it is unclear which are due
to this fix and what if due to my environment, so we stash this for now.

script.c
@@ -240,7 +240,7 @@ char   *buf1;                           /* Pointer to resolved buffer*/
 /*-------------------------------------------------------------------*/
 /* Function to build system configuration                            */
 /*-------------------------------------------------------------------*/
-DLL_EXPORT int process_config (char *cfg_name)
+DLL_EXPORT int process_config (const char *cfg_name)
 {
 #ifdef EXTERNALGUI
 char buf[1024];                         /* Config statement buffer   */

bldcfg.c
@@ -68,7 +68,7 @@
 /*-------------------------------------------------------------------*/
 /* Function to build system configuration                            */
 /*-------------------------------------------------------------------*/
-int build_config (char *hercules_cnf)
+int build_config (const char *hercules_cnf)
 {
 int     i;                              /* Array subscript           */
 int     devtmax;                        /* Max number device threads */

hexterns.h
@@ -262,10 +262,10 @@ HSYS_DLL_IMPORT int extgui;             // __attribute__ ((deprecated));
 #endif /*EXTERNALGUI*/
 
 /* Functions in module bldcfg.c */
-int build_config (char *fname);
+int build_config (const char *fname);
 
 /* Functions in module script.c */
-SCRI_DLL_IMPORT int process_config (char *fname);
+SCRI_DLL_IMPORT int process_config (const char *fname);
 SCRI_DLL_IMPORT int parse_args (char* p, int maxargc, char** pargv, int* pargc);
 
 /* Functions in module config.c */

msgenu.h
@@ -966,7 +966,7 @@ Examples:
 #define HHC01404 "Cannot create the Automatic Operator thread"
 #define HHC01405 "Script file %s not found"
 #define HHC01406 "Startup parm -l: maximum loadable modules %d exceeded; remainder not loaded"
-#define HHC01407 "Usage: %s [-f config-filename] [-r rcfile-name] [-d] [-b logo-filename] [-s sym=val] [-t [factor]]%s [> logfile]"
+#define HHC01407 "Usage: %s [-f config-filename] [-r rcfile-name] [-d] [-b logo-filename]%s [-t [factor]]%s [> logfile]"
 #define HHC01408 "Hercules terminating, see previous messages for reason"
 #define HHC01409 "Load of dyngui.dll failed, hercules terminated"
 #define HHC01410 "Cannot register %s handler: %s"
@@ -1401,6 +1401,8 @@ Examples:
 #define HHC02339 "Script %d: test: duration limit: %"PRId32".%06"PRId32" seconds"
 #define HHC02340 "Script %d: test: cannot initalise semaphore: %s"
 #define HHC02341 "Script %d: test: unknown runtest keyword: %s"
+#define HHC02342 "%s file '%s' not found:  %s"
+#define HHC02343 "Terminating due to %d argument errors"
 
 // range 02350 - 02369 available
 

tests/runtest
@@ -62,8 +62,8 @@ function verlt()
 
 wfn=allTests
 
-if [ ${FUNCNAME[ (( ${#FUNCNAME[@]} - 1 ))]} = "source" ] ; then
-   echo "Do not source $0."
+if [ ${FUNCNAME[0]} == "source" ] ; then
+   echo "Do not source $BASH_SOURCE"
    return 20
 fi
 
@@ -153,7 +153,7 @@ fi
 # a log file.  2>&1 disables the panel display and the ability to enter
 # panel commands.
 
-cmd="$hdir/hercules -f $dir/tests.conf -r $wfn.testin -t"
+cmd="$hdir/hercules -d -f $dir/tests.conf -r $wfn.testin -t"
 if [ "$exit" = "noexit" ] ; then
         $cmd >$wfn.out
         rv=$?

commit 971a8645922c483d36c62e8214bafa5cf94f0d14

Use hercules.rc in current directory if appropriate:
Fix regression caused by commit 79a0a17354d9f5ca9ce6032d3448aad3fca59ce2: use the .rc file specified by the '-r' command line argument first (if given), or defined by the HERCULES_RC environment variable if the '-r' command line argument was not specified, but then default to using the "hercules.rc" file in the current directory (if it exists) if neither was specified.

Closes #179: "Presence of hercules.rc not honored, need -r option".

impl.c
@@ -79,7 +79,7 @@ static char*  modnames[ MAX_MODS ] = {0};       /* ptrs to modnames  */
 static int    modcount = 0;                     /* count of modnames */
 
 /* forward define process_script_file (ISW20030220-3) */
-extern int process_script_file(char *,int);
+extern int process_script_file( const char*, bool );
 /* extern int quit_cmd(int argc, char *argv[],char *cmdline);        */
 
 /* Forward declarations:                                             */
@@ -529,29 +529,50 @@ DLL_EXPORT  COMMANDHANDLER  getCommandHandler()
 
 /*-------------------------------------------------------------------*/
 /* Process .RC file thread.                                          */
-/*                                                                   */
-/* Called synchronously when in daemon mode.                         */
+/* Called synchronously when in daemon mode. Else asynchronously.    */
 /*-------------------------------------------------------------------*/
-static void* process_rc_file (void* dummy)
+static void* process_rc_file( void* dummy )
 {
-    char  pathname[MAX_PATH];
+    char  pathname[ MAX_PATH ];
 
-    UNREFERENCED(dummy);
+    UNREFERENCED( dummy );
 
-    /* We have a .rc file to run */
-    hostpath(pathname, cfgorrc[want_rc].filename, sizeof(pathname));
+    /* Obtain the name of the hercules.rc file or default */
+    if (0
+        /* Neither '-r' nor "HERCULES_RC" environment variable given */
+        || !cfgorrc[want_rc].filename
+        || !cfgorrc[want_rc].filename[0]
+    )
+    {
+        /* Check for "hercules.rc" file in current directory */
+        struct stat st;
+        if (stat( "hercules.rc", &st ) == 0)
+            cfgorrc[want_rc].filename = "hercules.rc";
+    }
 
-    /* Wait for panel thread to engage */
-// ZZ FIXME:THIS NEEDS TO GO
-    if (!sysblk.daemon_mode)
-        while (!sysblk.panel_init)
-            usleep( 10 * 1000 );
+    /* If we have a hercules.rc file, process it */
+    if (1
+        && cfgorrc[want_rc].filename
+        && cfgorrc[want_rc].filename[0]
+        && strcasecmp( cfgorrc[want_rc].filename, "None" ) != 0
+    )
+    {
+        hostpath( pathname, cfgorrc[want_rc].filename, sizeof( pathname ));
 
-    /* Run the script processor for this file */
-    process_script_file(pathname, 1);
-        // (else error message already issued)
+#if 1 // ZZ FIXME: THIS NEEDS TO GO
 
-    return NULL;    /* End the .rc thread */
+        /* Wait for panel thread to engage */
+        if (!sysblk.daemon_mode)
+            while (!sysblk.panel_init)
+                usleep( 10 * 1000 );
+
+#endif // ZZ FIXME: THIS NEEDS TO GO
+
+        /* Run the script processor for this file */
+        process_script_file( pathname, true );
+    }
+
+    return NULL;
 }
 
 /*-------------------------------------------------------------------*/
@@ -1194,17 +1215,14 @@ int     rc;
 
     /* Process the .rc file synchronously when in daemon mode. */
     /* Otherwise Start up the RC file processing thread.       */
-    if (cfgorrc[want_rc].filename)
+    if (sysblk.daemon_mode)
+        process_rc_file( NULL );
+    else
     {
-        if (sysblk.daemon_mode)
-            process_rc_file( NULL );
-        else
-        {
-            rc = create_thread( &rctid, DETACHED,
-                process_rc_file, NULL, "process_rc_file" );
-            if (rc)
-                WRMSG( HHC00102, "E", strerror( rc ));
-        }
+        rc = create_thread( &rctid, DETACHED,
+            process_rc_file, NULL, "process_rc_file" );
+        if (rc)
+            WRMSG( HHC00102, "E", strerror( rc ));
     }
 
     if (log_callback)
@@ -1231,7 +1249,7 @@ int     rc;
         else
         {
             /* daemon mode without any daemon_task */
-            process_script_file("-", 1);
+            process_script_file( "-", true );
 
             /* We come here only if the user did ctl-d on a tty,
                or we reached EOF on stdin.  No quit command has

script.c
@@ -722,7 +722,7 @@ static int script_abort( SCRCTL* pCtl )
 /*-------------------------------------------------------------------*/
 /* Process a single script file                 (internal, external) */
 /*-------------------------------------------------------------------*/
-int process_script_file( char *script_name, const int isrcfile )
+int process_script_file( const char* script_name, bool isrcfile )
 {
 SCRCTL* pCtl;                           /* Script processing control */
 char   *scrname;                        /* Resolved script name      */
@@ -740,14 +740,13 @@ int     rc;                             /* (work)                    */
     {
         /* If not found it's probably the Hercules ".RC" file */
         ASSERT( isrcfile );
+
         /* Create a temporary working control entry */
         if (!(pCtl = NewSCRCTL( tid, script_name )))
             return -1; /* (error message already issued) */
 
-        /* Start  over again using our temporary control entry.  The */
-        /* screwy second argument is to silence the unused parameter */
-        /* warning when ASSERT does no assert (the mind boggles)     */
-        rc = process_script_file( script_name, 0 & isrcfile );
+        /* Start over again using our temporary control entry. */
+        rc = process_script_file( script_name, isrcfile );
         FreeSCRCTL( pCtl );
         return rc;
     }
@@ -918,7 +917,7 @@ static void *script_thread( void *arg )
     for (i=1; !script_abort( pCtl ) && i < argc; i++)
     {
         UpdSCRCTL( pCtl, argv[i] );
-        process_script_file( argv[i], 0 );
+        process_script_file( argv[i], false );
     }
 
     /* Remove entry from list and exit */
@@ -962,7 +961,7 @@ int script_cmd( int argc, char* argv[], char* cmdline )
         for (i=1; !script_abort( pCtl ) && i < argc; i++)
         {
             UpdSCRCTL( pCtl, argv[i] );
-            rc = process_script_file( argv[i], 0 );
+            rc = process_script_file( argv[i], false );
             if (0 <= rc2 && 0 < rc) rc2 = MAX( rc, rc2 );
             else if (0 > rc) rc2 = MIN( rc, rc2 );
         }