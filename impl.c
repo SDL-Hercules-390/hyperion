SDL-Hercules-390/hyperion

commit 38d1785f2733ec91b9d24f425ea39c87a1060f5b

Replace 'getenv' calls with 'get_symbol' calls instead:
Closes GitHub Issue #21.

impl.c
@@ -1550,7 +1550,7 @@ error:
         for (i=0; cfgorrccount > i; i++)
         {
             if (!cfgorrc[i].filename)       /* No value specified */
-                cfgorrc[i].filename = getenv( cfgorrc[i].envname );
+                cfgorrc[i].filename = get_symbol( cfgorrc[i].envname );
 
             if (!cfgorrc[i].filename)       /* No environment var */
             {

codepage.c
@@ -1083,7 +1083,7 @@ DLL_EXPORT void set_codepage( const char* name )
     if(name == NULL)
     {
         dflt = TRUE;
-        if(!(name = getenv("HERCULES_CP")))
+        if(!(name = get_symbol("HERCULES_CP")))
              name = "default";
     }
 

console.c
@@ -1867,15 +1867,13 @@ static BYTE* buffer_addsf( BYTE* bfr, size_t* buflen, size_t* alloc_len, BYTE at
 /*-------------------------------------------------------------------*/
 static void init_logo()
 {
-char   *p;            /* pointer logo filename */
-int     rc = 0;
-char    fn[FILENAME_MAX] = { 0 };
+    const char*  p; /* pointer to logo filename */
+    int          rc = 0;
+    char         fn[FILENAME_MAX] = {0};
 
     if (sysblk.logofile == NULL) /* LogoFile NOT passed in command line */
     {
-        p = getenv("HERCLOGO");
-
-        if ( p == NULL)
+        if (!(p = get_symbol("HERCLOGO")))
             p = "herclogo.txt";
     }
     else

getopt.c
@@ -94,7 +94,7 @@ extern char __declspec(dllimport) *__progname;
 /* This differs from the cygwin implementation, which effectively defaults to
    PC, but is consistent with the NetBSD implementation and doc's.  */
 #ifndef IS_POSIXLY_CORRECT
-#define IS_POSIXLY_CORRECT (getenv("POSIXLY_CORRECT") != NULL)
+#define IS_POSIXLY_CORRECT (get_symbol("POSIXLY_CORRECT") != NULL)
 #endif
 
 #define PERMUTE         (!IS_POSIXLY_CORRECT && !IGNORE_FIRST)

hRexx.c
@@ -270,36 +270,36 @@ void InitializeRexx()      // (called by impl.c at system startup)
 /*-------------------------------------------------------------------*/
 static void InitializeDefaults()
 {
-    char* envvar;
+    const char* sym;
 
     /* Retrieve and save the user's preferred default values */
 
-    if (!(envvar = getenv( "HREXX_PACKAGE" )))
+    if (!(sym = get_symbol( "HREXX_PACKAGE" )))
         DefaultPackage = strdup( DEFAULT_PACKAGE );
     else
-        DefaultPackage = strdup( envvar );
+        DefaultPackage = strdup( sym );
 
-    if (!(envvar = getenv( "HREXX_MODE" )))
+    if (!(sym = get_symbol( "HREXX_MODE" )))
         DefaultMode = DEFAULT_MODE;
     else
     {
-        if (strcasecmp( envvar, "Command" ) == 0)
+        if (strcasecmp( sym, "Command" ) == 0)
             DefaultMode = MODE_COMMAND;
-        else if (strcasecmp( envvar, "Subroutine" ) == 0)
+        else if (strcasecmp( sym, "Subroutine" ) == 0)
             DefaultMode = MODE_SUBROUTINE;
         else
             DefaultMode = DEFAULT_MODE;
     }
 
-    if (!(envvar = getenv( "HREXX_PATH" )))
+    if (!(sym = get_symbol( "HREXX_PATH" )))
         DefaultRexxPath = strdup( DEFAULT_REXXPATH );
     else
-        DefaultRexxPath = strdup( envvar );
+        DefaultRexxPath = strdup( sym );
 
-    if (!(envvar = getenv( "HREXX_EXTENSIONS" )))
+    if (!(sym = get_symbol( "HREXX_EXTENSIONS" )))
         DefaultExtensions = strdup( DEFAULT_EXTENSIONS );
     else
-        DefaultExtensions = strdup( envvar );
+        DefaultExtensions = strdup( sym );
 
     /* Initialize the defaults */
 
@@ -313,7 +313,7 @@ static void InitializeDefaults()
 /*-------------------------------------------------------------------*/
 /* InitializePaths and InitializeExtensions helper function          */
 /*-------------------------------------------------------------------*/
-static void InitArray( char* str, char* delims, char* def,
+static void InitArray( const char* str, const char* delims, const char* def,
                        BYTE empty0, /* array[0] == empty string? */
                        char** arraystr, char*** array, int* count )
 {
@@ -383,7 +383,7 @@ static void InitializePaths( char* wPath )
     InitArray( wPath, PATHDELIM, DefaultRexxPath, empty0,
         &RexxPath, &RexxPathArray, &RexxPathCount );
 
-    InitArray( getenv( "PATH" ), PATHDELIM, NULL, empty0,
+    InitArray( get_symbol( "PATH" ), PATHDELIM, NULL, empty0,
         &SysPath, &SysPathArray, &SysPathCount );
 }
 

hconsole.c
@@ -923,7 +923,8 @@ int console_beep( FILE* confp )
 
 int  get_console_dim( FILE* confp, int* rows, int* cols )
 {
-    char* env;
+    const char* sym;
+
 #if defined(TIOCGWINSZ)
     struct winsize winsize;
 #else
@@ -945,10 +946,10 @@ int  get_console_dim( FILE* confp, int* rows, int* cols )
     else
 #endif
     {
-        if (!(env = getenv( "LINES"   ))) *rows = 24;
-        else                              *rows = atoi(env);
-        if (!(env = getenv( "COLUMNS" ))) *cols = 80;
-        else                              *cols = atoi(env);
+        if (!(sym = get_symbol( "LINES"   ))) *rows = DEFAULT_CONSOLE_ROWS;
+        else                                  *rows = atoi( sym );
+        if (!(sym = get_symbol( "COLUMNS" ))) *cols = DEFAULT_CONSOLE_COLS;
+        else                                  *cols = atoi( sym );
     }
 
     if (!*rows || !*cols)

hconsole.h
@@ -9,6 +9,9 @@
 #ifndef _HCONSOLE_H
 #define _HCONSOLE_H
 
+#define DEFAULT_CONSOLE_ROWS    24
+#define DEFAULT_CONSOLE_COLS    80
+
 //-----------------------------------------------------------------------------
 //
 //                            VT100 User Guide

hdl.c
@@ -787,10 +787,10 @@ DLL_EXPORT void hdl_initpath( const char* path )
     {
         // 2) HERCULES_LIB environment variable
 
-        char* def;
+        const char* def;
         char pathname[ MAX_PATH ];
 
-        if ((def = getenv("HERCULES_LIB")))
+        if ((def = get_symbol("HERCULES_LIB")))
         {
             hostpath( pathname, def, sizeof( pathname ));
             hdl_modpath = strdup( pathname );

commit c0ae4c8dead999b92a4886081015c1b0f57c8e46

Use default hercules.cnf in current directory if appropriate:
Yet ANOTHER fix needed to correct the fallout caused by commit 38d1785f2733ec91b9d24f425ea39c87a1060f5b!!

impl.c
@@ -1567,29 +1567,33 @@ error:
 
         for (i=0; cfgorrccount > i; i++)
         {
-            if (!cfgorrc[i].filename)       /* No value specified */
+            /* If no value explicitly specified, try env. default */
+            if (!cfgorrc[i].filename)
                 cfgorrc[i].filename = get_symbol( cfgorrc[i].envname );
 
-            if (!cfgorrc[i].filename)       /* No environment var */
+            /* If no env. default, try hard coded default */
+            if (0
+                || !cfgorrc[i].filename
+                || !cfgorrc[i].filename[0]
+            )
             {
-                if (!(rv = stat( cfgorrc[i].defaultfile, &st )))
+                /* Use default from current directory if it exists */
+                if ((rv = stat( cfgorrc[i].defaultfile, &st )) == 0)
                     cfgorrc[i].filename = cfgorrc[i].defaultfile;
                 continue;
             }
 
-            if (0
-                || !cfgorrc[i].filename[0]     /* Null name */
-                || !strcasecmp( cfgorrc[i].filename, "None" )
-            )
+            /* Explicit request for no file use at all? */
+            if (strcasecmp( cfgorrc[i].filename, "None" ) == 0)
             {
                cfgorrc[i].filename = NULL;  /* Suppress file */
                continue;
             }
 
-            /* File specified explicitly or by environment */
-            if (-1 == (rv = stat(cfgorrc[i].filename, &st)))
+            /* File specified explicitly or by env; check existence */
+            if ((rv = stat( cfgorrc[i].filename, &st )) != 0)
             {
-                // "%s file %s not found:  %s"
+                // "%s file %s not found: %s"
                 WRMSG( HHC02342, "S", cfgorrc[i].whatfile,
                     cfgorrc[i].filename, strerror( errno ));
                 arg_error++;

msgenu.h
@@ -1424,7 +1424,7 @@ LOGM_DLL_IMPORT int  panel_command_capture( char* cmd, char** resp );
 #define HHC02339 "Script %d: test: duration limit: %"PRId32".%06"PRId32" seconds"
 //efine HHC02340 (available)
 #define HHC02341 "Script %d: test: unknown runtest keyword: %s"
-#define HHC02342 "%s file '%s' not found:  %s"
+#define HHC02342 "%s file '%s' not found: %s"
 #define HHC02343 "Terminating due to %d argument errors"
 #define HHC02344 "%s device %1d:%04X group has registered MAC address %s"
 #define HHC02345 "%s device %1d:%04X group has registered IP address %s"