SDL-Hercules-390/hyperion

commit df0f6d26725236bcd9ef254a2d11b48232c1d3b3

Shared DASD Server fix for RCD NED serial number issue
Closes #243

msgenu.h
@@ -626,7 +626,7 @@ LOGM_DLL_IMPORT int  panel_command_capture( char* cmd, char** resp );
 #define HHC00713 "%1d:%04X Shared: error during channel program start"
 #define HHC00714 "%1d:%04X Shared: error during channel program end"
 #define HHC00715 "%1d:%04X Shared: remote error reading track %d"
-//efine HHC00716 (available)
+#define HHC00716 "%1d:%04X Shared: error retrieving serial number"
 #define HHC00717 "%1d:%04X Shared: error retrieving usage information"
 #define HHC00718 "%1d:%04X Shared: error writing track %d"
 #define HHC00719 "%1d:%04X Shared: remote error writing track %d %2.2X-%2.2X"

shared.c
@@ -275,6 +275,18 @@ init_retry:
     }
     dev->numdevid = rc;
 
+    /* Get the serial number */
+    rc = clientRequest (dev, dev->serial, sizeof(dev->serial),
+                        SHRD_QUERY, SHRD_SERIAL, NULL, NULL);
+    if (rc < 0)
+        goto init_retry;
+    else if (rc == 0 || rc > (int)sizeof(dev->serial))
+    {
+        // "%1d:%04X Shared: error retrieving serial number"
+        WRMSG( HHC00716, "S", LCSS_DEVNUM );
+        return -1;
+    }
+
     /* Indicate no active track */
     dev->cache = dev->bufcur = -1;
     dev->buf = NULL;
@@ -540,6 +552,18 @@ init_retry:
         return -1;
     }
     dev->numdevchar = rc;
+
+    /* Get the serial number */
+    rc = clientRequest (dev, dev->serial, sizeof(dev->serial),
+                        SHRD_QUERY, SHRD_SERIAL, NULL, NULL);
+    if (rc < 0)
+        goto init_retry;
+    else if (rc == 0 || rc > (int)sizeof(dev->serial))
+    {
+        // "%1d:%04X Shared: error retrieving serial number"
+        WRMSG( HHC00716, "S", LCSS_DEVNUM );
+        return -1;
+    }
 
     /* Indicate no active track */
     dev->cache = dev->bufcur = -1;
@@ -1956,6 +1980,11 @@ char     trcmsg[32];
         case SHRD_DEVID:
             SHRD_SET_HDR (hdr, 0, 0, dev->devnum, id, dev->numdevid);
             serverSend (dev, ix, hdr, dev->devid, dev->numdevid);
+            break;
+
+        case SHRD_SERIAL:
+            SHRD_SET_HDR (hdr, 0, 0, dev->devnum, id, sizeof( dev->serial ));
+            serverSend (dev, ix, hdr, dev->serial, (U32)sizeof( dev->serial ));
             break;
 
         case SHRD_CKDCYLS:

html/hercnew.html
@@ -29,17 +29,20 @@ Hercules: What's new
 
 <li> Make HHC02917 a suppressable msglevel debug only message
 <li> Fix CKD suffixed read count file protect bug
-<li> Provide dasd serial number support
+<li> CKD dasd serial number support
 <li> Fix various VMFPLC2 issues
 <li> Fix Linux exit/quit hang when no traffic on tuntap device
 <li> Fix QETH (OSA) missing CSCH interrupt under z/VM
 <li> Don't pass "&&" async argument to rexx scripts
 <li> Fix bogus HHC01111W warning and cctape display
+<li> hercifc network interface configuration utility fixes
+<li> SIE fix for z/VM z/Arch real mode guests (e.g. zcms)
+<li> 3215/1052 console missing interrupt fix
 <li> xxxxx
 <li> xxxxx
 <li> xxxxx
 <li> xxxxx
-<li> xxxxx
+<li> Various internal fixes and improvements
 
 </ul>
 

html/shared.html
@@ -11,11 +11,11 @@ Hercules: Shared Device Server
 <hr noshade>
 <h2>Contents</h2>
 <ul>
-<li><a href="#overview">     Overview      </a>
-<li><a href="#commands">     Commands      </a>
-<li><a href="#technical">    Technical BS  </a>
-<li><a href="#caching">      Caching       </a>
-<li><a href="#compression">  Compression   </a>
+<li><a href="#overview">     Overview               </a>
+<li><a href="#commands">     Commands               </a>
+<li><a href="#technical">    Technical Information  </a>
+<li><a href="#caching">      Caching                </a>
+<li><a href="#compression">  Compression            </a>
 </ul>
 
 <p>
@@ -140,7 +140,7 @@ command with no arguments displays the current setting. Use the command
 
 <p><br>
 
-<h2><a NAME="technical">Technical BS</a></h2>
+<h2><a NAME="technical">Technical Information</a></h2>
 <p>
 
 There are (at least) two approaches to sharing devices.  One is to
@@ -164,7 +164,7 @@ Both client and server have a DEVBLK structure for the device.
 Absurdly, perhaps, in originally designing an implementation for
 shared devices it was not clear what type of process should be the
 server.  It was a quantum leap forward to realize that it could
-just be another hercules instance.
+just be another Hercules instance.
 
 <p>
 
@@ -538,7 +538,7 @@ time is required.
 
 If the server is on 'localhost' then you should not specify 'comp='.
 Otherwise you are just stealing processor time to do compression/
-uncompression from hercules.  If the server is on a local network
+uncompression from Hercules.  If the server is on a local network
 then I would recommend specifying a low value such as 1, 2 or 3.
 We are on a curve here, trying to trade cpu cycles for network traffic
 to derive an optimal throughput.

shared.h
@@ -325,7 +325,7 @@
 /*-------------------------------------------------------------------*/
 
 #define SHARED_VERSION              0   /* Version level  (0 .. 15)  */
-#define SHARED_RELEASE              1   /* Release level  (0 .. 15)  */
+#define SHARED_RELEASE              2   /* Release level  (0 .. 15)  */
 
 /* Constraints                                                       */
 #define SHARED_DEFAULT_PORT      3990   /* Default shared port       */
@@ -383,6 +383,7 @@
 #define SHRD_DEVCHAR             0x41   /* Device characteristics    */
 #define SHRD_DEVID               0x42   /* Device identifier         */
 #define SHRD_USED                0x43   /* Device usage              */
+#define SHRD_SERIAL              0x44   /* Device serial number      */
 #define SHRD_CKDCYLS             0x48   /* CKD number cylinders      */
 #define SHRD_FBAORIGIN           0x4c   /* FBA origin                */
 #define SHRD_FBANUMBLK           0x4d   /* FBA number blocks         */

commit 55d369c86771cd9299a01672115798bf67ce51b4

Fix shared dasd server query serial incompatibility bug:
Commit df0f6d26725236bcd9ef254a2d11b48232c1d3b3 accidentally introduced an incompatibility with older shared dasd servers (e.g. shared dasd servers older than version 0.2, such as e.g. Hercules 3.11 running shared dasd server version 0.1, etc) wherein if the server was 0.1 but the client was 0.2, the opening of dasds would fail because the 0.2 client was sending a query serial number request to the 0.1 server which the 0.1 server didn't understand. The fix is to adjust the 0.2 client to NOT send queries for the serial number to servers older than 0.2.

Bug reported by Bob Schmidt (rgschmi@execpc.com).  Thank you, Bob!

hstructs.h
@@ -1254,6 +1254,7 @@ struct DEVBLK {                         /* Device configuration block*/
         U16     rmtport;                /* Remote port number        */
         U16     rmtnum;                 /* Remote device number      */
         int     rmtid;                  /* Remote Id                 */
+        int     rmtver;                 /* Remote version level      */
         int     rmtrel;                 /* Remote release level      */
         DBLWRD  rmthdr;                 /* Remote header             */
         int     rmtcomp;                /* Remote compression parm   */

msgenu.h
@@ -631,7 +631,7 @@ LOGM_DLL_IMPORT int  panel_command_capture( char* cmd, char** resp );
 #define HHC00718 "%1d:%04X Shared: error writing track %d"
 #define HHC00719 "%1d:%04X Shared: remote error writing track %d %2.2X-%2.2X"
 #define HHC00720 "%1d:%04X Shared: error in function %s: %s"
-#define HHC00721 "%1d:%04X Shared: connected to file %s"
+#define HHC00721 "%1d:%04X Shared: connected to v%d.%d server id %d file %s"
 #define HHC00722 "%1d:%04X Shared: error in connect to file %s: %s"
 #define HHC00723 "%1d:%04X Shared: error in send for %2.2X-%2.2X: %s"
 #define HHC00724 "%1d:%04X Shared: not connected to file %s"

shared.c
@@ -16,6 +16,7 @@ DISABLE_GCC_UNUSED_FUNCTION_WARNING;
 #include "opcode.h"
 #include "devtype.h"
 #include "ccwarn.h"
+#include "dasdblks.h"
 
 DISABLE_GCC_UNUSED_SET_WARNING;
 
@@ -275,16 +276,27 @@ init_retry:
     }
     dev->numdevid = rc;
 
-    /* Get the serial number */
-    rc = clientRequest (dev, dev->serial, sizeof(dev->serial),
-                        SHRD_QUERY, SHRD_SERIAL, NULL, NULL);
-    if (rc < 0)
-        goto init_retry;
-    else if (rc == 0 || rc > (int)sizeof(dev->serial))
+    /* Get the serial number if the server supports such a query */
+
+    if (dev->rmtver <  SHARED_VERSION ||
+       (dev->rmtver == SHARED_VERSION &&
+        dev->rmtrel <  SHARED_RELEASE))
     {
-        // "%1d:%04X Shared: error retrieving serial number"
-        WRMSG( HHC00716, "S", LCSS_DEVNUM );
-        return -1;
+        /* Generate a random serial number */
+        gen_dasd_serial( dev->serial );
+    }
+    else /* (server SHOULD support the SHRD_SERIAL query) */
+    {
+        rc = clientRequest (dev, dev->serial, sizeof(dev->serial),
+                            SHRD_QUERY, SHRD_SERIAL, NULL, NULL);
+        if (rc < 0)
+            goto init_retry;
+        else if (rc == 0 || rc > (int)sizeof(dev->serial))
+        {
+            // "%1d:%04X Shared: error retrieving serial number"
+            WRMSG( HHC00716, "S", LCSS_DEVNUM );
+            return -1;
+        }
     }
 
     /* Indicate no active track */
@@ -553,16 +565,27 @@ init_retry:
     }
     dev->numdevchar = rc;
 
-    /* Get the serial number */
-    rc = clientRequest (dev, dev->serial, sizeof(dev->serial),
-                        SHRD_QUERY, SHRD_SERIAL, NULL, NULL);
-    if (rc < 0)
-        goto init_retry;
-    else if (rc == 0 || rc > (int)sizeof(dev->serial))
+    /* Get the serial number if the server supports such a query */
+
+    if (dev->rmtver <  SHARED_VERSION ||
+       (dev->rmtver == SHARED_VERSION &&
+        dev->rmtrel <  SHARED_RELEASE))
     {
-        // "%1d:%04X Shared: error retrieving serial number"
-        WRMSG( HHC00716, "S", LCSS_DEVNUM );
-        return -1;
+        /* Generate a random serial number */
+        gen_dasd_serial( dev->serial );
+    }
+    else /* (server SHOULD support the SHRD_SERIAL query) */
+    {
+        rc = clientRequest (dev, dev->serial, sizeof(dev->serial),
+                            SHRD_QUERY, SHRD_SERIAL, NULL, NULL);
+        if (rc < 0)
+            goto init_retry;
+        else if (rc == 0 || rc > (int)sizeof(dev->serial))
+        {
+            // "%1d:%04X Shared: error retrieving serial number"
+            WRMSG( HHC00716, "S", LCSS_DEVNUM );
+            return -1;
+        }
     }
 
     /* Indicate no active track */
@@ -1151,12 +1174,14 @@ HWORD              comp;                /* Returned compression parm */
                 {
                     dev->connected = 1;             // (SHRD_CONNECT success)
                     dev->rmtid  = fetch_hw( id );   // (must only do ONCE!!!)
-                    dev->rmtrel = flag & 0x0f;
+                    dev->rmtver = flag >> 4;        // (save server version)
+                    dev->rmtrel = flag & 0x0f;      // (save server release)
 
                     if (!dev->batch)
                         if (MLVL( VERBOSE ))
-                            // "%1d:%04X Shared: connected to file %s"
-                            WRMSG( HHC00721, "I", LCSS_DEVNUM, dev->filename );
+                            // "%1d:%04X Shared: connected to v%d.%d server id %d file %s"
+                            WRMSG( HHC00721, "I", LCSS_DEVNUM, dev->rmtver,
+                                dev->rmtrel, dev->rmtid, dev->filename );
                     /*
                      * Negotiate compression - top 4 bits have the compression
                      * algorithms we support (00010000 -> libz; 00100000 ->bzip2,
@@ -1168,7 +1193,6 @@ HWORD              comp;                /* Returned compression parm */
                      * cckd or cfba then the server doesn't have to uncompress
                      * the data for us if we support the compression algorithm.
                      */
-
                     if (dev->rmtcomp || dev->rmtcomps)
                     {
                         rc = clientRequest( dev, comp, 2, SHRD_COMPRESS,