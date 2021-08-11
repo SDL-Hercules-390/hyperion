cc71855f8677fcbb0cc1323a3efb37f1605dfc81
Fish (David B. Trout) <fish@infidels.org>
2021-08-10 18:27:07 -0700

    (cosmetic) [skip travis]

--- hinlines.h
+++ hinlines.h
@@ -8,48 +8,36 @@
 #ifndef _HINLINES_H
 #define _HINLINES_H
 
+/*-------------------------------------------------------------------*/
 /* Define inline assembly style for GNU C compatible compilers */
-#if defined(__GNUC__)
+
+#if defined( __GNUC__ )
     #define asm  __asm__
 #endif
 
 /*-------------------------------------------------------------------*/
 
-#if !defined(round_to_hostpagesize)
-static inline U64 round_to_hostpagesize(U64 n)
-{
-    U64 factor = hostinfo.hostpagesz - 1;
-    return ((n + factor) & ~factor);
-}
-#endif
+#if !defined( clear_io_buffer )
+
+#define clear_storage(   _addr, _n )  __clear_io_buffer( (void*)(_addr), (size_t)(_n) )
+#define clear_io_buffer( _addr, _n )  __clear_io_buffer( (void*)(_addr), (size_t)(_n) )
+
+#define clear_page(    _addr )        __clear_page( (void*)(_addr), (size_t)( FOUR_KILOBYTE / 64 ) )
+#define clear_page_1M( _addr )        __clear_page( (void*)(_addr), (size_t)( ONE_MEGABYTE  / 64 ) )
+#define clear_page_4K( _addr )        __clear_page( (void*)(_addr), (size_t)( FOUR_KILOBYTE / 64 ) )
+#define clear_page_2K( _addr )        __clear_page( (void*)(_addr), (size_t)( TWO_KILOBYTE  / 64 ) )
 
 /*-------------------------------------------------------------------*/
 
-#if !defined(clear_io_buffer)
-
-#define clear_storage(_addr, _n)                                       \
-      __clear_io_buffer((void *)(_addr), (size_t)(_n))
-#define clear_io_buffer(_addr,_n)                                      \
-      __clear_io_buffer((void *)(_addr),(size_t)(_n))
-#define clear_page(_addr)                                              \
-      __clear_page((void *)(_addr), (size_t)( FOUR_KILOBYTE / 64 ) )
-#define clear_page_1M(_addr)                                           \
-      __clear_page((void *)(_addr), (size_t)( ONE_MEGABYTE  / 64 ) )
-#define clear_page_4K(_addr)                                           \
-      __clear_page((void *)(_addr), (size_t)( FOUR_KILOBYTE / 64 ) )
-#define clear_page_2K(_addr)                                           \
-      __clear_page((void *)(_addr), (size_t)( TWO_KILOBYTE  / 64 ) )
-
-/*-------------------------------------------------------------------*/
-
-#if defined(__GNUC__) && defined(__SSE2__) && (__SSE2__ == 1)
+#if defined( __GNUC__) && defined( __SSE2__ ) && ( __SSE2__ == 1 )
   #define _GCC_SSE2_
-#elif defined (_MSVC_)
+#elif defined ( _MSVC_ )
   #if defined( _M_X64 )
-    /* "On the x64 platform, __faststorefence generates
-        an instruction that is a faster store fence
-        than the sfence instruction. Use this intrinsic
-        instead of _mm_sfence on the x64 platform."
+    /*
+        Microsoft x64: "On the x64 platform, __faststorefence
+        generates an instruction that is a faster store fence
+        than the sfence instruction. Use this intrinsic instead
+        of _mm_sfence on the x64 platform."
     */
     #define  SFENCE  __faststorefence
   #else
@@ -59,20 +47,23 @@ static inline U64 round_to_hostpagesize(U64 n)
 
 /*-------------------------------------------------------------------*/
 
-/* #if defined(_GCC_SSE2_) */
-#if 0
+#if defined( _GCC_SSE2_ )
 
-static inline void __clear_page( void *addr, size_t pgszmod64 )
+static inline void __clear_page( void* addr, size_t pgszmod64 )
 {
     unsigned char xmm_save[16];
     unsigned int i;
 
     asm volatile("": : :"memory");      /* barrier */
 
+    /* Init work reg to 0 */
+
     asm volatile("\n\t"
         "movups %%xmm0, (%0)\n\t"
         "xorps  %%xmm0, %%xmm0"
         : : "r" (xmm_save));
+
+    /* Clear requested page WITHOUT polluting our cache */
 
     for (i = 0; i < pgszmod64; i++)
         asm volatile("\n\t"
@@ -82,17 +73,18 @@ static inline void __clear_page( void *addr, size_t pgszmod64 )
             "movntps %%xmm0, 48(%0)"
             : : "r"(addr) : "memory");
 
+    /* An SFENCE guarantees that every preceding store
+       is globally visible before any subsequent store. */
+
     asm volatile("\n\t"
         "sfence\n\t"
         "movups (%0), %%xmm0"
         : : "r" (xmm_save) : "memory");
-
-    return;
 }
 
 /*-------------------------------------------------------------------*/
 
-#elif defined (_MSVC_)
+#elif defined ( _MSVC_ )
 
 static inline void __clear_page( void* addr, size_t pgszmod64 )
 {
@@ -109,8 +101,8 @@ static inline void __clear_page( void* addr, size_t pgszmod64 )
     xmm0 = _mm_setzero_ps();        // (suppresses C4700; will be optimized out)
     _mm_xor_ps( xmm0, xmm0 );
 
-    /* Clear requested page without polluting our cache */
-    for (i=0; i < pgszmod64; i++, (float*) addr += 16 )
+    /* Clear requested page WITHOUT polluting our cache */
+    for (i=0; i < pgszmod64; i++, (float*) addr += 16)
     {
         _mm_stream_ps( (float*) addr+ 0, xmm0 );
         _mm_stream_ps( (float*) addr+ 4, xmm0 );
@@ -121,19 +113,17 @@ static inline void __clear_page( void* addr, size_t pgszmod64 )
     /* An SFENCE guarantees that every preceding store
        is globally visible before any subsequent store. */
     SFENCE();
-
-    return;
 }
 #else /* (all others) */
-  #define  __clear_page(_addr, _pgszmod64 )    memset((void*)(_addr), 0, ((size_t)(_pgszmod64)) << 6)
+  #define  __clear_page( _addr, _pgszmod64 )    memset( (void*)(_addr), 0, ((size_t)(_pgszmod64)) << 6 )
 #endif
 
 /*-------------------------------------------------------------------*/
 
-#if defined(_GCC_SSE2_)
-static inline void __optimize_clear(void *addr, size_t n)
+#if defined( _GCC_SSE2_ )
+static inline void __optimize_clear( void* addr, size_t n )
 {
-    char *mem = addr;
+    char* mem = addr;
 
     /* Let the compiler perform special case optimization */
     while (n-- > 0)
@@ -142,16 +132,16 @@ static inline void __optimize_clear(void *addr, size_t n)
     return;
 }
 #else /* (all others, including _MSVC_) */
-  #define __optimize_clear(p,n)     memset((void*)(p),0,(size_t)(n))
+  #define __optimize_clear( p, n )  memset( (void*)(p), 0, (size_t)(n) )
 #endif
 
 /*-------------------------------------------------------------------*/
 
-#if defined(_GCC_SSE2_) || defined (_MSVC_)
-static inline void __clear_io_buffer(void *addr, size_t n)
+#if defined( _GCC_SSE2_ ) || defined ( _MSVC_ )
+static inline void __clear_io_buffer( void* addr, size_t n )
 {
     unsigned int x;
-    void *limit;
+    void* limit;
 
     /* Let the C compiler perform special case optimization */
     if ((x = (U64)(uintptr_t)addr & 0x00000FFF))
@@ -159,10 +149,9 @@ static inline void __clear_io_buffer(void *addr, size_t n)
         unsigned int a = 4096 - x;
 
         __optimize_clear( addr, a );
+
         if (!(n -= a))
-        {
             return;
-        }
     }
 
     /* Calculate page clear size */
@@ -183,9 +172,7 @@ static inline void __clear_io_buffer(void *addr, size_t n)
 
     /* Clean up any remainder */
     if (n)
-    {
         __optimize_clear( addr, n );
-    }
 
     return;
 
@@ -194,10 +181,10 @@ static inline void __clear_io_buffer(void *addr, size_t n)
 /*-------------------------------------------------------------------*/
 
 #else /* (all others) */
-  #define  __clear_io_buffer(_addr, _n)  memset((void*)(_addr), 0, (size_t)(_n))
+  #define  __clear_io_buffer( _addr, _n )  memset( (void*)(_addr), 0, (size_t)(_n) )
 #endif
 
-#endif /* !defined(clear_io_buffer) */
+#endif /* !defined( clear_io_buffer ) */
 
 
 /*-------------------------------------------------------------------*/
