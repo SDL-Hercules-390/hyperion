SDL-Hercules-390/hyperion

commit e9e1a2a59b2cc86d0c9cbe5eb9f21ded7b641130

Fix day-one bug in STSTU (Search String Unicode) instruction

general2.c
@@ -2912,7 +2912,7 @@ DEF_INST( search_string_unicode )
     CONTRAN_INSTR_CHECK( regs );
 
     /* Program check if bits 0-15 of register 0 not zero */
-    if(regs->GR_L(0) & 0xFFFF0000)
+    if (regs->GR_L(0) & 0xFFFF0000)
         regs->program_interrupt( regs, PGM_SPECIFICATION_EXCEPTION );
 
     /* Load string terminating character from register 0 bits 16-31 */
@@ -2922,12 +2922,27 @@ DEF_INST( search_string_unicode )
     addr1 = regs->GR( r1 ) & ADDRESS_MAXWRAP( regs );
     addr2 = regs->GR( r2 ) & ADDRESS_MAXWRAP( regs );
 
-    /* Search up to 256 bytes or until end of operand */
-    for(i = 0; i < 0x100; i++)
+    /* "When the contents of bit position 63 of general registers R1
+        and R2 differ (that is, when one address is even and the other
+        is odd), the location of the first two-byte character after
+        the second operand is ONE MORE THAN the contents of general
+        register R1." (emphasis added)
+    */
+    if ((addr1 & 1) != (addr2 & 1))
+    {
+        addr1++;
+        addr1 &= ADDRESS_MAXWRAP( regs );
+    }
+
+#undef  SRSTU_MAX
+#define SRSTU_MAX   _4K     /* (Sheesh! 256 bytes is WAY too small!) */
+
+    /* Search up to CPU-determined bytes or until end of operand */
+    for (i=0; i < SRSTU_MAX; i++)
     {
         /* If operand end address has been reached, return condition
-        code 2 and leave the R1 and R2 registers unchanged */
-        if(addr2 == addr1)
+           code 2 and leave the R1 and R2 registers unchanged */
+        if (addr2 >= addr1)
         {
             regs->psw.cc = 2;
             return;
@@ -2937,8 +2952,8 @@ DEF_INST( search_string_unicode )
         sbyte = ARCH_DEP( vfetch2 )( addr2, r2, regs );
 
         /* If the terminating character was found, return condition
-        code 1 and load the address of the character into R1 */
-        if(sbyte == termchar)
+           code 1 and load the address of the character into R1 */
+        if (sbyte == termchar)
         {
             SET_GR_A( r1, regs, addr2 );
             regs->psw.cc = 1;

tests/SRSTU.tst
@@ -1,0 +1,17 @@
+*Testcase SRSTU: Simple Search String Unicode test
+sysclear
+archlvl z/Arch
+
+r 1a0=00000001800000000000000000000200  # z/Arch restart PSW
+r 1d0=0002000180000000000000000000DEAD  # z/Arch pgm new PSW
+
+r 200=41000fff                          # GR0 = character to search for
+r 204=41200300                          # R2 --> string to be searched
+r 208=41100401                          # R1 --> end of string
+r 20c=B9BE0012                          # SRSTU
+r 210=4710020c                          # CC3: keep searching...
+r 214=B2B20218                          # LPSWE GOODPSW
+r 218=00020001800000000000000000000000  # GOODPSW
+
+runtest .1
+*Done

commit 89f494add0b0faa480a8a021668048d588f1ca06

Fix bug introduced in commit e9e1a2a59b2cc86d0c9cbe5eb9f21ded7b641130...
... and add comments to prevent re-occurrence.

general2.c
@@ -583,6 +583,12 @@ BYTE    termchar;                       /* Terminating character     */
                because if the boundary condition is met,
                there is no further need to access storage.
             */
+            /* NOTE: "When the address in general register R1 is less
+               than the address in general register R2, condition code
+               2 can be set only if the operand wraps around from the
+               top of storage to location 0."  The below comparison
+               for == is thus correct.
+            */
             if (addr2 == addr1)
             {
                 regs->psw.cc = 2;
@@ -592,7 +598,14 @@ BYTE    termchar;                       /* Terminating character     */
             for (i=0; i < dist; i++)
             {
                 /* If operand end address has been reached, return
-                   CC=2 and leave the R1 and R2 registers unchanged */
+                   CC=2 and leave the R1 and R2 registers unchanged
+                */
+                /* NOTE: "When the address in general register R1 is
+                   less than the address in general register R2, then
+                   condition code 2 can be set only when the operand
+                   wraps around from the top of storage to location 0."
+                   Thus the below == comparison is correct.
+                */
                 if (addr2 == addr1)
                 {
                     regs->psw.cc = 2;
@@ -2942,7 +2955,13 @@ DEF_INST( search_string_unicode )
     {
         /* If operand end address has been reached, return condition
            code 2 and leave the R1 and R2 registers unchanged */
-        if (addr2 >= addr1)
+
+        /* NOTE: "When the address in general register R1 is less than
+           the address in general register R2, condition code 2 can be
+           set only if the operand wraps around from the top of storage
+           to location 0."  Thus the below == comparison is correct.
+        */
+        if (addr2 == addr1)
         {
             regs->psw.cc = 2;
             return;