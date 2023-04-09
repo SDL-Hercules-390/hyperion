SDL-Hercules-390/hyperion

commit 2dc33fe76d9a2126935fde63c71b5ad1986eb0e6

TXF SIE fixes:
 * Fix TXF Program Interrupt filtering logic and add sie_exit handshaking.
 * sie_exit: add missing "Interception TDB" logic.

[skip travis]

(Skipping travis because this is just one of several commits in a long series of commits. The last commit in the series will allow travis to run.)

sie.c
@@ -1212,6 +1212,12 @@ endloop:        ; // (nop to make compiler happy)
 void ARCH_DEP( sie_exit )( REGS* regs, int icode )
 {
     int n;
+#if defined( FEATURE_073_TRANSACT_EXEC_FACILITY )
+    U64  itdba = 0;
+    TDB* itdb = NULL;
+    bool txf_contran = false;
+    BYTE txf_tnd = 0;
+#endif
 
     PTT_SIE( "sie_xit i,h,g", icode, regs->host, regs->guest );
 
@@ -1290,7 +1296,7 @@ void ARCH_DEP( sie_exit )( REGS* regs, int icode )
     STATEBK->f = 0;
 
     /* Set the interception code in the SIE block */
-    switch (icode > 0 ? icode & ~PGM_PER_EVENT : icode)
+    switch (icode < 0 ? icode : icode & 0xFF)
     {
        /* If host interrupt pending, then backup psw so that the SIE
           instruction gets re-executed again to re-enter SIE mode
@@ -1319,6 +1325,100 @@ void ARCH_DEP( sie_exit )( REGS* regs, int icode )
         case PGM_OPERATION_EXCEPTION:  STATEBK->c = SIE_C_OPEREXC;  break;
         default:                       STATEBK->c = SIE_C_PGMINT;   break;
     }
+
+    PTT_SIE( "sie_xit STA_c", STATEBK->c, 0, 0  );
+
+#if defined( FEATURE_073_TRANSACT_EXEC_FACILITY )
+
+    /* US 8,880,959 B2, Greiner et al, 17.20:
+    
+       "Interception TDB: The 256-byte host real location 
+        specified by locations 488-495 (x'1E8') of the state
+        description."
+    */
+    FETCH_DW( itdba, STATEBK->itdba );
+    if (itdba)
+        itdba = APPLY_PREFIXING( itdba, HOSTREGS->PX );
+
+    PTT_TXF( "TXF itdb", itdba, 0, 0 );
+
+    OBTAIN_TXFLOCK( GUESTREGS );
+    {
+        txf_tnd     = GUESTREGS->txf_tnd;
+        txf_contran = GUESTREGS->txf_contran;
+    }
+    RELEASE_TXFLOCK( GUESTREGS );
+
+    PTT_TXF( "TXF tnd,con", txf_tnd, txf_contran, 0 );
+
+    /* "A transaction may be aborted due to causes that
+        are outside the scope of the immediate configuration
+        in which it executes. For example, transient events
+        recognized by a hypervisor (such as LPAR or z/VM)
+        may cause a transaction to be aborted."
+    */
+    if (txf_tnd)
+    {
+        PTT_TXF( "TXF abrt", 0, 0, TAC_MISC );
+
+        GUESTREGS->txf_why |= TXF_WHY_SIE_EXIT;
+        ARCH_DEP( abort_transaction )( GUESTREGS, ABORT_RETRY_RETURN, TAC_MISC );
+        itdb = txf_contran ? &GUESTREGS->txf_pi_tdb
+                           : &GUESTREGS->txf_tb_tdb;
+    }
+    else if (GUESTREGS->txf_UPGM_abort)
+    {
+        PTT_TXF( "TXF upgm", GUESTREGS->txf_UPGM_abort, GUESTREGS->txf_caborts, 0 );
+
+        itdb = GUESTREGS->txf_caborts ? &GUESTREGS->txf_pi_tdb
+                                      : &GUESTREGS->txf_tb_tdb;
+    }
+
+    /* Check if we need to fill in an Interception TDB */
+    if (STATEBK->c == SIE_C_PGMINT && itdba && itdb)
+    {
+        PTT_TXF( "TXF itdb <=", itdba, itdb, itdb->tdb_tac );
+
+        /* US 8,880,959 B2, Greiner et al, 17.20:
+
+           "The interception TDB is stored when an aborted
+            transaction results in a guest program interruption
+            interception (i.e. interception code 8).  When a
+            transaction is aborted due to other causes, the
+            contents of the interception TDB are unpredictable."
+        */
+        if (TXF_TRACING())
+        {
+            // "TXF: %s%02X: SIE: Populating Interception TDB at 0x%16.16"PRIx64
+            WRMSG( HHC17714, "D", TXF_CPUAD( GUESTREGS ), itdba );
+        }
+
+        memcpy( HOSTREGS->mainstor + itdba, itdb, sizeof( TDB ));
+    }
+    else if (STATEBK->c == SIE_C_PGMINT && itdb)
+    {
+        PTT_TXF( "*TXF !itdba", itdba, itdb, itdb->tdb_tac );
+
+        if (TXF_TRACING())
+        {
+            // "TXF: %s%02X: SIE: Interception TDB address not provided!"
+            WRMSG( HHC17716, "D", TXF_CPUAD( GUESTREGS ));
+        }
+    }
+    else if (STATEBK->c != SIE_C_PGMINT && itdba)
+    {
+        PTT_TXF( "*TXF itdba<=0", itdba, 0, 0 );
+
+        /* The address of an Interception TDB was provided, but no
+           transaction was aborted as a result of this intercepted
+           program interrupt. For safety, return a "NULL" (empty)
+           Interception TDB instead. (Sorry Dan! Could not locate
+           Claudia Schiffer’s phone number!)
+        */
+        memset( HOSTREGS->mainstor + itdba, 0, sizeof( TDB ));
+    }
+
+#endif /* defined( FEATURE_073_TRANSACT_EXEC_FACILITY ) */
 
     /* Save CPU timer  */
     STORE_DW( STATEBK->cputimer, cpu_timer( GUESTREGS ));

cpu.c
@@ -441,17 +441,62 @@ char    dxcstr[8] = {0};                /* " DXC=xx" if data excptn  */
     code = pcode & ~PGM_PER_EVENT;
 
 #if defined( FEATURE_073_TRANSACT_EXEC_FACILITY )
-    ARCH_DEP( txf_do_pi_filtering )( realregs, &pcode, code );
-    /* Abort any active transaction w/unfiltered program interrupt and
-       return to here to continue with program interrupt processing */
-    PTT_TXF( "TXF UPGM", realregs, 0, realregs->txf_tnd );
+
+    /* If transaction is active, check if interrupt can be filtered */
     if (realregs->txf_tnd)
     {
-        PTT_TXF( "*TXF UPGM", realregs, ABORT_RETRY_RETURN, TAC_UPGM );
-        ARCH_DEP( abort_transaction )( realregs, ABORT_RETRY_RETURN, TAC_UPGM );
-        ilc = REAL_ILC( realregs );
+        /* Indicate TXF related program interrupt */
+        pcode |= PGM_TXF_EVENT;
+
+        /* Save the program interrupt and data exception codes */
+        realregs->txf_piid   = pcode;
+        realregs->txf_piid  |= (ilc << 16);
+
+        realregs->txf_dxc_vxc =
+        (0
+            || pcode == PGM_DATA_EXCEPTION
+            || pcode == PGM_VECTOR_PROCESSING_EXCEPTION
+        )
+        ?  realregs->dxc : 0;
+
+        PTT_TXF( "TXF PIID", realregs->txf_piid, realregs->txf_dxc_vxc, 0 );
+
+        /* 'txf_do_pi_filtering' does not return for filterable
+            program interrupts. It returns only if the program
+            interrupt is unfilterable, and when it returns, the
+            active transaction has already been aborted.
+        */
+
+        PTT_TXF( "TXF PROG?", (code & 0xFF), realregs->txf_contran, realregs->txf_tnd );
+
+        ARCH_DEP( txf_do_pi_filtering )( realregs, (code & 0xFF) );
+
+        if (realregs->txf_tnd ) // (sanity check)
+            CRASH();            // (sanity check)
+
+        PTT_TXF( "*TXF UPROG!", (code & 0xFF), 0, 0 );
+
+        /* Set flag for sie_exit */
+        realregs->txf_UPGM_abort = true;
     }
-#endif
+    else /* (no transaction is CURRENTLY active) */
+    {
+        /* While no transaction is CURRENTLY active, it's possible
+           that a previously active transaction was aborted BEFORE
+           we were called, so we need to check for that.
+        */
+        if ((code & 0xFF) == PGM_TRANSACTION_CONSTRAINT_EXCEPTION)
+        {
+            /* Indicate TXF related program interrupt */
+            pcode |= PGM_TXF_EVENT;
+
+            PTT_TXF( "*TXF 218!", pcode, 0, 0 );
+
+            /* Set flag for sie_exit */
+            realregs->txf_UPGM_abort = true;
+        }
+    }
+#endif /* defined( FEATURE_073_TRANSACT_EXEC_FACILITY ) */
 
     /* If this is a concurrent PER event
        then we must add the PER bit to the interrupts code */

hexterns.h
@@ -280,7 +280,7 @@ TRANS_DLL_IMPORT BYTE* txf_maddr_l( const U64  vaddr,   const size_t  len,
 TRANS_DLL_IMPORT void s370_abort_transaction( REGS* regs, int retry, int txf_tac );
 TRANS_DLL_IMPORT void s390_abort_transaction( REGS* regs, int retry, int txf_tac );
 TRANS_DLL_IMPORT void z900_abort_transaction( REGS* regs, int retry, int txf_tac );
-TRANS_DLL_IMPORT void z900_txf_do_pi_filtering( REGS* regs, int* pcode, int code );
+TRANS_DLL_IMPORT void z900_txf_do_pi_filtering( REGS* regs, int code );
 void alloc_txfmap( REGS* regs );
 void free_txfmap( REGS* regs );
 void txf_abort_all( U16 cpuad, int why, const char* location );

hstructs.h
@@ -359,7 +359,6 @@ struct REGS {                           /* Processor registers       */
         TDB     txf_tb_tdb;             /* TBEGIN instruction TDB    */
 
         bool    txf_NTSTG;              /* true == NTSTG instruction */
-        bool    txf_aborted;            /* true == aborted trans.    */
         bool    txf_contran;            /* true == CONSTRAINED mode  */
         bool    txf_cfail;              /* true == CONSTRAINED failed*/
         bool    txf_UPGM_abort;         /* true == transaction was

transact.c
@@ -1452,7 +1452,7 @@ VADR       txf_atia = PSW_IA( regs, -REAL_ILC( regs ) );
 /* normal program interrupt processing.                              */
 /*                                                                   */
 /*-------------------------------------------------------------------*/
-DLL_EXPORT void ARCH_DEP( txf_do_pi_filtering )( REGS* regs, int* pcode, int code )
+DLL_EXPORT void ARCH_DEP( txf_do_pi_filtering )( REGS* regs, int code )
 {
 bool    filt;                   /* true == filter the interrupt      */
 int     txclass;                /* Transactional Execution Class     */
@@ -1464,19 +1464,13 @@ int     fcc, ucc;               /* Filtered/Unfiltered conditon code */
     regs->txf_NTSTG = false;
 
     /* No filtering if no transaction was active */
-    if (!regs->txf_aborted)
+    if (!regs->txf_tnd)
     {
         PTT_TXF( "*TXF !filt", code, regs->txf_contran, regs->txf_tnd );
         return;
     }
 
-    /* (reset flag) */
-    regs->txf_aborted = false;
-
-    /* Indicate TXF aborted event in interrupt code */
-    *pcode |= PGM_TXF_EVENT;
-
-    switch (code)  // (interrupt code)
+    switch (code & 0xFF)  // (interrupt code)
     {
     case PGM_OPERATION_EXCEPTION:
     case PGM_PRIVILEGED_OPERATION_EXCEPTION:
@@ -1649,6 +1643,7 @@ int     fcc, ucc;               /* Filtered/Unfiltered conditon code */
     regs->psw.cc = ucc;
     PTT_TXF( "TXF unfilt!", code, regs->txf_contran, regs->txf_tnd );
     regs->txf_why |= TXF_WHY_UNFILT_INT;
+    ARCH_DEP( abort_transaction )( regs, ABORT_RETRY_RETURN, TAC_UPGM );
 
 } /* end txf_do_pi_filtering */
 #endif /* defined( FEATURE_073_TRANSACT_EXEC_FACILITY ) */

commit 376d445f00ae0beb4a96a3c4cde41cf399589daf

Fix careless typo introduced by commit 2dc33fe76d9a2126935fde63c71b5ad1986eb0e6
Closes #560

sie.c
@@ -1483,7 +1483,7 @@ void ARCH_DEP( sie_exit )( REGS* regs, int icode )
     STATEBK->f = 0;
 
     /* Set the interception code in the SIE block */
-    switch (icode < 0 ? icode : icode & 0xFF)
+    switch (icode < 0 ? icode : icode & 0x7F)
     {
        /* If host interrupt pending, then backup psw so that the SIE
           instruction gets re-executed again to re-enter SIE mode