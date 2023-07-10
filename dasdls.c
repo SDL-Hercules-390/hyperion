8132a50aa6beaad1a22998dac4d5b6afe7c48c39
Fish (David B. Trout) <fish@infidels.org>
2023-07-09 17:24:09 -0700

    (fix line-endings)

--- dasdls.c
+++ dasdls.c
@@ -1,5 +1,5 @@
 /* DASDLS.C    (C) Copyright Roger Bowler, 1999-2012                 */
-/*             (C) and others 2013-2022                              */
+/*             (C) and others 2013-2023                              */
 /*              Hercules DASD Utilities: DASD list program           */
 /*                                                                   */
 /*   Released under "The Q Public License Version 1"                 */
